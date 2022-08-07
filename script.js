373b3f446a0e37f4558919a6997b3a726a3e65f3
extf8 <extfield8@gmail.com>
2022-08-07 18:31:06 +0200

    Added func to color pickers n canvas size pickers

--- script.js
+++ script.js
@@ -7,6 +7,55 @@ let currentColor = default_color;
 let currentMode = default_mode;
 
 const grid = document.getElementById('grid-container');
+const colorPick = document.getElementById('color-pick');
+const colorBtn = document.getElementById('color-btn');
+const rgbBtn = document.getElementById('rgb-btn');
+const eraseBtn = document.getElementById('erase-btn');
+const clearBtn = document.getElementById('clear-btn');
+const smlSizeBtn = document.getElementById('sml-size-btn');
+const medSizeBtn = document.getElementById('med-size-btn');
+const lgeSizeBtn = document.getElementById('lge-size-btn');
+
+colorPick.oninput = (e) => setNewColor(e.target.value);
+colorBtn.onclick = () => setMode('color');
+rgbBtn.onclick = () => setMode('rgb');
+eraseBtn.onclick = () => setMode('erase');
+clearBtn.onclick = () => reloadGrid();
+smlSizeBtn.onclick = (e) => changeSize(e.target.value);
+medSizeBtn.onclick = (e) => changeSize(e.target.value);
+lgeSizeBtn.onclick = (e) => changeSize(e.target.value);
+
+function setNewColor(newColor) {
+    currentColor = newColor
+}
+
+function setMode(newMode) {
+    selectedBtn(newMode)
+    currentMode = newMode
+}
+
+function reloadGrid() {
+    clearGrid()
+    createGrid(currentSize)
+}
+
+function clearGrid() {
+    grid.innerHTML = ''
+}
+
+function setSize(newSize) {
+    currentSize = newSize
+}
+
+function changeSize(value) {
+    setSize(value)
+    reloadGrid()
+}
+
+function reloadGrid() {
+    clearGrid()
+    createGrid(currentSize)
+}
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
@@ -30,10 +79,35 @@ function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
     if (currentMode === 'color') {
         e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'rgb') {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'erase') {
+        e.target.style.backgroundColor = '#fefefe'
     }
 }
 
+function selectedBtn(newMode) {
+    if (currentMode === 'rgb') {
+      rgbBtn.classList.remove('selected-btn')
+    } else if (currentMode === 'color') {
+      colorBtn.classList.remove('selected-btn')
+    } else if (currentMode === 'erase') {
+      eraseBtn.classList.remove('selected-btn')
+    }
+  
+    if (newMode === 'rgb') {
+      rgbBtn.classList.add('selected-btn')
+    } else if (newMode === 'color') {
+      colorBtn.classList.add('selected-btn')
+    } else if (newMode === 'erase') {
+      eraseBtn.classList.add('selected-btn')
+    }
+  }
 
 window.onload = () => {
     createGrid(default_size);
+    selectedBtn(default_mode);
 };  
